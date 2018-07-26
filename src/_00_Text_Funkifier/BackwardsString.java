a94e75f7f3fd327087148980df437fd2e274211b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-25 18:26:24 -0700

    I worked on _00_Text_Funkifier. I completed #1,2, and 3. I will begin on #4 next week.

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -8,8 +8,18 @@ public class BackwardsString extends SpecialString {
 
 	@Override
 	public String funkifyText(String s) {
+		String finalText = ""; 
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			
+			finalText = c + finalText; 
+			
 		
-		return "";
+			
+		}
+		
+
+		return finalText;
 	}
 
 }
--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -9,7 +9,30 @@ public class MixedCapsString extends SpecialString{
 	@Override
 	public String funkifyText(String s) {
 		
-		return "";
+		String finalText = ""; 
+		
+		for (int i = 0; i < s.length(); i++) {
+			
+			char c = s.charAt(i);
+			
+			if(i%2==1) { //Odd; lower
+				c = Character.toUpperCase(c);
+			}
+			else { //Even; upper
+				c = Character.toLowerCase(c);
+				
+			}
+			
+			
+			finalText = finalText + c; 
+			
+		
+			
+		}
+		
+
+		return finalText;
+	
 	}
 
 }
